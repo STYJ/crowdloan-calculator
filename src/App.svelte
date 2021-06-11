8d4ed87ab7744d6b6cec1c61258471f2c0eba2b9
STYJ <simon.tan.yu.jing@gmail.com>
2021-06-11 18:26:36 +0800

    added full source code

---
+++ src/App.svelte
@@ -0,0 +1,100 @@
+
+<script>
+	import RangeSlider from "svelte-range-slider-pips";
+	let ticker = "SDN";
+	let ploTokenAllocation = 15400000;
+	let ksmCollected = 500000;
+	let prices = [5];
+	let totalSupply = 70000000;
+	let ksmPrice = 500;
+
+	var formatter = new Intl.NumberFormat('en-US', {
+  		style: 'currency',
+  		currency: 'USD',
+	});
+</script>
+
+
+<main>
+	<h1>Crowdloan Calculator <span class="sub">Made by <a href="https://twitter.com/itsmeSTYJ">Simon</a></span></h1>
+	
+	<p>Estimate your returns from participating in the Polkadot / Kusama parachain auction!</p>
+
+	<h3>0. Input token details</h3>
+	<div>
+		<label for="ticker">Token Symbol:</label>
+  		<input type="text" id="ticker" bind:value={ticker}>
+	</div>
+	<div>
+		<label for="totalSupply">Total Supply:</label>
+  		<input type="text" id="totalSupply" bind:value={totalSupply}>
+	</div>
+	
+	<h3>1. Input crowdloan variables</h3>
+	
+	<div>
+		<label for="ploTokenAllocation"># tokens allocated for PLO:</label>
+  		<input type="text" id="ploTokenAllocation" bind:value={ploTokenAllocation}>
+	</div>
+	<div>
+		<label for="ksmCollected"># KSM collected so far:</label>
+  		<input type="text" id="ksmCollected" bind:value={ksmCollected}>
+	</div>
+	<div>
+		<label for="rate">Current {ticker}/KSM rate:</label>
+		<span id="rate">{+(ploTokenAllocation / ksmCollected).toFixed(2)}</span>
+	</div>
+	<span class="sub">The calculation above assumes no bonuses.</span>
+
+	<h3>2. Input estimated price of {ticker}</h3>
+	<div>
+		<label for="price">Price:</label>
+		<div style="display: inline-block; width: 200px;">
+			<RangeSlider float=true max="500" bind:values={prices} prefix="$"/>
+			<input type="text" id="price" max=500 bind:value={prices[0]}>		
+		</div>
+	</div>
+	<div>
+		<label for="marketCap">Estimated market cap:</label>
+		<span id="marketCap"> {formatter.format(prices[0] * totalSupply)}</span>
+	</div>
+	<h3>3. Input current price of KSM and calculate returns</h3>
+	<div>
+		<label for="ksmPrice">Current KSM price:</label>
+  		<input type="text" id="ksmPrice" bind:value={ksmPrice}>
+	</div>
+	<div>
+		<label for="estimatedReturns">Estimated returns:</label>
+		<span id="estimatedReturns">{+(ploTokenAllocation / ksmCollected * prices[0] * 100/ksmPrice).toFixed(2)}%</span>
+	</div>
+	<span class="sub">Formula = rate * token price * 100 / ksm price</span>
+	<h3>4. Decide if crowdloan returns + X months lock is better than staking KSM for 12% returns + 1 week lock</h3>
+</main>
+
+<style>
+	main {
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+	}
+
+	label {
+		display: contents;
+	}
+
+	.sub {
+		color: grey;
+		font-size: x-small;
+	}
+
+	@media (min-width: 480px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
